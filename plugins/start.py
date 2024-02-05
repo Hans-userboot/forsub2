1807213412f514cd4f3f314176b7cafc272cdbac
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2024-02-06 00:54:52 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,7 @@ from config import (
     PROTECT_CONTENT,
     START_MSG,
 )
-from database.sql import add_user, delete_user, full_userbase, query_msg
+from database.mongo import add_user, delete_user, full_userbase, query_msg
 from pyrogram import filters
 from pyrogram.enums import ParseMode
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked
